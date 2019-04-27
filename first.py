1316be470693338d02b3bcfb2cbbecc16d73f3a4
felixhornfeldt <felix.hornfeldt@outlook.com>
2019-04-27 12:01:31 +0200

    updated first.py

--- first.py
+++ first.py
@@ -1,11 +1,25 @@
-a = ["Hello", "world", "from", "array"]
+# a = ["Hello", "world", "from", "array"]
 
 
-def msg():
-    op = 0
-    for b in a:
-        op += 1
-        print(b)
-    print(str(op))
+# def msg():
+#     op = 0
+#     for b in a:
+#         op += 1
+#         print(b)
+#     print(str(op))
 
-msg()
+# msg()
+
+def arr_sum(arr, n):
+    e = []
+    for x in arr:
+        if x in e:
+            return True
+        elif (n-x) in e:
+            continue
+        else:
+            e.append(n-x)
+    
+    return False
+
+print(arr_sum([9,1,1,0], 9))
